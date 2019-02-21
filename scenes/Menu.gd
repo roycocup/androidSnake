f309012ba124322b7991b7291ec274e90d34dc61
Rodrigo Dias <rodrigo.dias@browsergroup.com>
2019-02-21 16:30:07 +0000

    pausing and unpausing game with ui

---
+++ scenes/Menu.gd
@@ -0,0 +1,13 @@
+extends Container
+
+onready var panel = $Panel
+
+func position_me():
+	var mid_x = panel.rect_size.x / 2
+	var mid_y = panel.rect_size.y / 2
+	var win_mid = Vector2(429 / 2, 768 / 2)
+	rect_position = Vector2(win_mid.x-mid_x, win_mid.y-mid_y)
+
+func _ready():
+	position_me()
+	
--- scripts/World.gd
+++ scripts/World.gd
@@ -6,17 +6,29 @@ func _ready():
 func _process(delta):
 	check_quit()
 	check_pause()
-	pass
+	check_menu()
 
 func check_quit():
-	if Input.is_key_pressed(KEY_ESCAPE):
+	if Input.is_action_just_pressed('ui_cancel'):
 		get_tree().quit()
 		
 
 func check_pause():
 	if Input.is_action_just_pressed('ui_pause'):
-		print(get_tree().paused)
-		if get_tree().paused == false:
-			get_tree().paused = true;
+		pause_game()
+
+func pause_game():
+	if get_tree().paused == false:
+		get_tree().paused = true;
+	else:
+		get_tree().paused = false;
+			
+func check_menu():
+	if Input.is_action_just_pressed('ui_show_hide'):
+		var menu = get_node('/root/World/Menu')
+		if menu.visible:
+			menu.visible = false
+			pause_game()
 		else:
-			get_tree().paused = false;
+			menu.visible = true
+			pause_game()
